6d6753d9ad807a9b755e9522925d722aa8970d6e
grossmannjenna <grossmannjenna@gmail.com>
2024-03-02 12:23:39 -0500

    added comments

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from PIL import Image, ImageOps
 
 chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
 
+#read in and open image source file 
 imageFile = input("Please enter the exact path to an image file: ")
 im = Image.open(imageFile)
 
+#break up image path into an array and store image file name
 fileName = imageFile.split("/")[-1]
-# fileName[-1] should be fileName
+#take off image type 
 fileName = fileName.split(".")[0] 
 
+#set image size
 x_dimension, y_dimension = im.size[0], im.size[1]
 
 x, y = 21, y_dimension - 42
 
+
 print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
 
+#convert to grayscale
 im = ImageOps.grayscale(im)
+
 
 im.save("test.jpg")
 
