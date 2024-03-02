f1a60d3b59dce280e46f564ecc0d3b9ca9a0320f
GC; Pins <spot.meow.meow@gmail.com>
2024-03-02 12:28:05 -0500

    updated

--- main.py
+++ main.py
@@ -2,47 +2,30 @@ from PIL import Image, ImageOps
 
 chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
 
-<<<<<<< Updated upstream
-#read in and open image source file 
-imageFile = input("Please enter the exact path to an image file: ")
-im = Image.open(imageFile)
 
-#break up image path into an array and store image file name
-fileName = imageFile.split("/")[-1]
-#take off image type 
-fileName = fileName.split(".")[0] 
-
-#set image size
-x_dimension, y_dimension = im.size[0], im.size[1]
-=======
 def main():
 
+      #read in and open image source file 
     imageFile = input("Please enter the exact path to an image file: ")
     im = Image.open(imageFile)
 
+    #break up image path into an array and store image file name
     fileName = imageFile.split("/")[-1]
-    # fileName[-1] should be fileName
+    #take off image type 
     fileName = fileName.split(".")[0] 
->>>>>>> Stashed changes
 
+    #set image size
     x_dimension, y_dimension = im.size[0], im.size[1]
-
-<<<<<<< Updated upstream
-
-print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
-
-#convert to grayscale
-im = ImageOps.grayscale(im)
+        #convert to grayscale
+    im = ImageOps.grayscale(im)
 
 
-im.save("test.jpg")
-=======
+    im.save("test.jpg")
     x, y = 21, y_dimension - 42
 
     print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
 
     im = ImageOps.grayscale(im)
->>>>>>> Stashed changes
 
     im.save("test.jpg")
 
