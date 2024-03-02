5da97b1f69b6213f41ea8217fcee2f0079bc7e83
GC; Pins <spot.meow.meow@gmail.com>
2024-03-02 12:26:14 -0500

    resolved conflicts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageOps
 
 chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
 
+<<<<<<< Updated upstream
 #read in and open image source file 
 imageFile = input("Please enter the exact path to an image file: ")
 im = Image.open(imageFile)
@@ -13,9 +14,20 @@ fileName = fileName.split(".")[0]
 
 #set image size
 x_dimension, y_dimension = im.size[0], im.size[1]
+=======
+def main():
 
-x, y = 21, y_dimension - 42
+    imageFile = input("Please enter the exact path to an image file: ")
+    im = Image.open(imageFile)
 
+    fileName = imageFile.split("/")[-1]
+    # fileName[-1] should be fileName
+    fileName = fileName.split(".")[0] 
+>>>>>>> Stashed changes
+
+    x_dimension, y_dimension = im.size[0], im.size[1]
+
+<<<<<<< Updated upstream
 
 print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
 
@@ -24,14 +36,27 @@ im = ImageOps.grayscale(im)
 
 
 im.save("test.jpg")
+=======
+    x, y = 21, y_dimension - 42
 
-print("Pixel at (1, 1): ", im.getpixel((x, y)))
+    print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
 
-asciiStr = ""
+    im = ImageOps.grayscale(im)
+>>>>>>> Stashed changes
 
-for i in range(0, x_dimension):
-    im.putpixel((x,y), (0))
-for i in range(0, y_dimension):
-    im.putpixel((x,y), (0))
+    im.save("test.jpg")
 
-im.save("cursed.jpg")
+    print("Pixel: ", im.getpixel((x, y)))
+
+    asciiStr = ""
+
+    for i in range(0, x_dimension):
+        for j in range(0, y_dimension):
+            #im.putpixel((i,j), 0)
+            asciiStr += getCharFromPixel(im.getpixel((i,j)))    
+
+    im.save("cursed.jpg")
+
+def getCharFromPixel(p):
+    
+    return 'a'
