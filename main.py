04db4f09f10fcbcfeda31f47cc7d27e23669bab6
GC; Pins <spot.meow.meow@gmail.com>
2024-03-02 11:23:11 -0500

    testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageOps
 
+chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
+
 imageFile = input("Please enter the exact path to an image file: ")
 im = Image.open(imageFile)
 
@@ -9,7 +11,7 @@ fileName = fileName.split(".")[0]
 
 x_dimension, y_dimension = im.size[0], im.size[1]
 
-x, y = 0, y_dimension - 1
+x, y = 21, y_dimension - 42
 
 print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Width: ", x_dimension, "px  Height: ", y_dimension, "px\n") 
 
@@ -17,6 +19,13 @@ im = ImageOps.grayscale(im)
 
 im.save("test.jpg")
 
-pixels = im.load()
+print("Pixel at (1, 1): ", im.getpixel((x, y)))
 
-print("Pixel at (1, 1): ", pixels[x, y])
+asciiStr = ""
+
+for i in range(0, x_dimension):
+    im.putpixel((x,y), (0))
+for i in range(0, y_dimension):
+    im.putpixel((x,y), (0))
+
+im.save("cursed.jpg")
