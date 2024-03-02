23686c8f7f6f4d6f0b7630cf9f3431493376d3e3
GC; Pins <spot.meow.meow@gmail.com>
2024-03-02 10:54:56 -0500

    convert to grayscale, random ops, etc.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageOps
 
 imageFile = input("Please enter the exact path to an image file: ")
 im = Image.open(imageFile)
 
-print(im.format, "\n Len: ", im.size[0], "px  Wid: ", im.size[1], "px\n") 
+fileName = imageFile.split("/")[-1]
+# fileName[-1] should be fileName
+fileName = fileName.split(".")[0] 
+
+x_dimension, y_dimension = im.size[0], im.size[1]
+
+x, y = 0, 0
+
+print("\n", "File Name: ", fileName, "\n", "Format: ", im.format, "\n Len: ", x_dimension, "px  Wid: ", y_dimension, "px\n") 
+
+im = ImageOps.grayscale(im)
+
+pixels = im.load()
+
+print("Pixel at (1, 1): ", pixels[x, y])
