a4167a79a84c6243c38ff8e625f3bb5922598ac3
GC; Pins <64855897+GCPins@users.noreply.github.com>
2024-03-04 01:18:22 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,57 +1,10 @@
 # main.py
 
 from flask import Flask, request, jsonify, render_template
-from PIL import Image, ImageOps
+from asciify import generate_ascii_art
 import os
 
 app = Flask(__name__)
-
-chars = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]
-
-def get_char_from_pixel(p):
-    if p < 25:
-        return chars[9]
-    elif p < 51:
-        return chars[8]
-    elif p < 76:
-        return chars[7]
-    elif p < 102:
-        return chars[6]
-    elif p < 127:
-        return chars[5]
-    elif p < 153:
-        return chars[4]
-    elif p < 178:
-        return chars[3]
-    elif p < 204:
-        return chars[2]
-    elif p < 229:
-        return chars[1]
-    elif p <= 255:
-        return chars[0]
-
-def generate_ascii_art(file_path, scale_factor):
-    try:
-        im = Image.open(file_path)
-        fileName = os.path.basename(file_path).split(".")[0]
-
-        x_dimension, y_dimension = im.size[0], im.size[1]
-        im = ImageOps.grayscale(im)
-
-
-        ascii_str = ""
-
-        im = im.resize((int(x_dimension / scale_factor), int(y_dimension / scale_factor)), resample=Image.LANCZOS)
-        x_dimension, y_dimension = im.size[0], im.size[1]
-
-        for i in range(0, y_dimension):
-            ascii_str += "\n"
-            for j in range(0, x_dimension):
-                ascii_str += str(get_char_from_pixel(im.getpixel((j, i))))
-
-        return ascii_str
-    except Exception as e:
-        return str(e)
 
 @app.route('/')
 def index():
